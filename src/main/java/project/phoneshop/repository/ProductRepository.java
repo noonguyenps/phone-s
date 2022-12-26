caa4ada713e022ae8fd6beb73f2ef586a4a65072
Admin <ngoctin2412test@gmail.com>
2022-12-26 22:33:24 +0700

    fix some bug order

--- src/main/java/project/phoneshop/repository/ProductRepository.java
+++ src/main/java/project/phoneshop/repository/ProductRepository.java
@@ -18,20 +18,20 @@ public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
             nativeQuery = true)
     Page<ProductEntity> findAllProduct(Pageable pageable);
     List<ProductEntity> findByProductCategory(CategoryEntity category);
-    @Query(value = "SELECT * FROM products WHERE category_id in ?1",
-            countQuery = "SELECT count(*) FROM products WHERE category_id in ?1",
+    @Query(value = "SELECT * FROM products WHERE category_id in ?1 and products.product_status=1",
+            countQuery = "SELECT count(*) FROM products WHERE category_id in ?1 and products.product_status=1",
             nativeQuery = true)
     Page<ProductEntity> findByCategory(List<UUID> categoryIds, Pageable pageable);
-    @Query(value = "SELECT * FROM products WHERE brand_id = ?1",
-            countQuery = "SELECT count(*) FROM products WHERE brand_id = ?1",
+    @Query(value = "SELECT * FROM products WHERE brand_id = ?1 and products.product_status=1",
+            countQuery = "SELECT count(*) FROM products WHERE brand_id = ?1 and products.product_status=1",
             nativeQuery = true)
     Page<ProductEntity> findByBrand(UUID brandId, Pageable pageable);
     @Query(value = "SELECT A.* FROM (SELECT * FROM products WHERE category_id in ?1) as A, (SELECT DISTINCT product_id FROM product_attribute_options WHERE id in ?2) as B WHERE A.product_id = B.product_id",
             countQuery = "SELECT A.* FROM (SELECT * FROM products WHERE category_id in ?1) as A, (SELECT DISTINCT product_id FROM product_attribute_options WHERE id in ?2) as B WHERE A.product_id = B.product_id",
             nativeQuery = true)
     Page<ProductEntity> findByAttributes(List<UUID> categoryIds, List<String>attribute, Pageable pageable);
-    @Query(value = "SELECT * FROM products WHERE LOWER(products.product_name) LIKE %?1%",
-            countQuery = "SELECT * FROM products WHERE LOWER(products.product_name) LIKE %?1%",
+    @Query(value = "SELECT * FROM products WHERE LOWER(products.product_name) LIKE %?1% and products.product_status=1",
+            countQuery = "SELECT * FROM products WHERE LOWER(products.product_name) LIKE %?1% and products.product_status=1",
             nativeQuery = true)
     Page<ProductEntity> findByKeyword(String keyword, Pageable pageable);
     @Query(value = "SELECT * FROM products WHERE products.product_status=?1",
