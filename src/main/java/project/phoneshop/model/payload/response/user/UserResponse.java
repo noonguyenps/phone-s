002c975cb7c4389d4ecdf10dc87ee6fb1878fd75
Admin <ngoctin2412test@gmail.com>
2022-12-31 11:29:05 +0700

    fix some bug order

--- src/main/java/project/phoneshop/model/payload/response/user/UserResponse.java
+++ src/main/java/project/phoneshop/model/payload/response/user/UserResponse.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Getter
 public class UserResponse {
     private UUID id;
-    private String fullname;
+    private String fullName;
     private String email;
     private String gender;
     private String nickName;
