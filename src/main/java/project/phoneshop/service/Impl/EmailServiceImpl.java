f0ba80a99fc52c2a023131140fe0a644e65897c4
Admin <ngoctin2412test@gmail.com>
2023-06-25 20:33:28 +0700

    update

--- src/main/java/project/phoneshop/service/Impl/EmailServiceImpl.java
+++ src/main/java/project/phoneshop/service/Impl/EmailServiceImpl.java
@@ -77,7 +77,7 @@ public class EmailServiceImpl implements EmailService {
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
         msg.setSubject("SPhone Reset password");
         msg.setContent("<!DOCTYPE html>\n" +
-                        "<html lang=\"en-US\">\n" +
+                        "<html lang=\"vi\">\n" +
                         "  <head>\n" +
                         "    <meta charset=\"utf-8\" />\n" +
                         "    <meta name=\"viewport\" content=\"width=device-width\" />\n" +
